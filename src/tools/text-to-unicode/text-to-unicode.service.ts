00a5762756b90cf0acf502462e86d3d88faba0b4
chengqing <chengqing@tencent.com>
2024-03-22 15:41:25 +0800

    fix: unicode

--- src/tools/text-to-unicode/text-to-unicode.service.ts
+++ src/tools/text-to-unicode/text-to-unicode.service.ts
@@ -1,9 +1,11 @@
 function convertTextToUnicode(text: string): string {
-  return text.split('').map(value => `&#${value.charCodeAt(0)};`).join('');
+  return text.split('').map(value => `\\u${value.charCodeAt(0).toString(16).padStart(4, '0')}`).join('');
 }
 
 function convertUnicodeToText(unicodeStr: string): string {
-  return unicodeStr.replace(/&#(\d+);/g, (match, dec) => String.fromCharCode(dec));
+  return unicodeStr.replace(/\\u[\dA-F]{4}/gi, (match) => {
+    return String.fromCharCode(Number.parseInt(match.replace(/\\u/g, ''), 16));
+  });
 }
 
 export { convertTextToUnicode, convertUnicodeToText };
